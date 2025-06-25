e296c9a0f5cc5a43c41b0b1efbd391b310643030
abdall2002 <abdallahaliahmed517@gmail.com>
2025-06-25 13:24:03 +0300

    PPP

---
+++ BethenyPieShop/Controllers/PieController.cs
@@ -0,0 +1,29 @@
+﻿using System.Runtime.CompilerServices;
+using BethenyPieShop.Models;
+using BethenyPieShop.ViewModels;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BethenyPieShop.Controllers
+{
+    public class PieController : Controller
+    {
+        private readonly IPieRepository _pieRepository;
+        private readonly ICategoryRepository _categoryRepository;
+
+        public PieController(IPieRepository pieRepository, ICategoryRepository categoryRepository)
+        {
+            _pieRepository = pieRepository;
+            _categoryRepository = categoryRepository;
+        }
+
+        public ActionResult List()
+        {
+            //ViewBag.CurrentCategory = "Cheese cake";
+            //return View(_pieRepository.AllPies);
+            PieListViewModel pieListViewModel = new PieListViewModel(_pieRepository.AllPies, "Cheese cake");
+            return View(pieListViewModel);
+
+        }
+
+    }
+}
